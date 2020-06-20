f2091036afc02b3ace4b6d433483e45499b59adf
leroykayanda <leroykayanda@gmail.com>
2020-06-20 20:37:55 +0200

    Update aws_bill_notifications_lambda.py

--- aws_bill_notifications_lambda.py
+++ aws_bill_notifications_lambda.py
@@ -4,8 +4,11 @@ from datetime import datetime,timedelta
 from dateutil import relativedelta
 import os
 
-def lambda_handler(event, context):    
+dollar_exchange_rate=float( os.environ['dollar_exchange_rate'] )
+
+def lambda_handler(event, context):  
     sendEmail()
+    #return predictedBill()
     
 def getMonthBill():
     client = boto3.client('ce')
@@ -31,7 +34,7 @@ def getMonthBill():
                  ]
         )
         
-    month_to_date_bill=round( float(response["ResultsByTime"][0]['Groups'][0]["Metrics"]["UnblendedCost"]["Amount"]) *102 )
+    month_to_date_bill=round( float(response["ResultsByTime"][0]['Groups'][0]["Metrics"]["UnblendedCost"]["Amount"]) * dollar_exchange_rate )
     return month_to_date_bill
     #return response
     
@@ -56,7 +59,8 @@ def getYesterdayBill():
                 }
                  ]
         )
-    yesterdays_bill=round(float(response["ResultsByTime"][0]["Groups"][0]['Metrics']["UnblendedCost"]["Amount"])*102) 
+    
+    yesterdays_bill=round(float(response["ResultsByTime"][0]["Groups"][0]['Metrics']["UnblendedCost"]["Amount"]))*dollar_exchange_rate 
     return yesterdays_bill
     #return response
     
@@ -80,12 +84,14 @@ def predictedBill():
         Granularity='MONTHLY'
         )
 
-    return round( float( response['Total']['Amount'] )*102 )
+    return round( float( response['Total']['Amount'] )* dollar_exchange_rate )
     #return response
     
 def sendEmail():
+    currency=os.environ['currency']
+    
     client = boto3.client('sns')
-    msg="AWS Account\n\nYesterday Usage : Kshs. "+format( getYesterdayBill() , ',d' )+" \n\nMonth To Date Bill : Kshs. "+format( getMonthBill(), ',d') +" \n\nForecasted Bill : Kshs. "+format(predictedBill(),',d')
+    msg="AWS Account\n\nYesterday Usage : "+currency+" "+ format( int( getYesterdayBill() ) ,',d' ) +" \n\nMonth To Date Bill : "+currency+" "+ format( int( getMonthBill() )  ,',d' )  +" \n\nForecasted Bill : "+currency+" "+ format( int( predictedBill() )  ,',d' ) 
     
     sns_topic=os.environ['sns_topic']
     subject=os.environ['subject']
